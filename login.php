475840f8efe5f599b47a73abcf8fe50193c7156c
ArnoBm <117790355+ArnoBm@users.noreply.github.com>
2022-12-03 15:51:07 +0600

    Update login.php

--- login.php
+++ login.php
@@ -12,18 +12,8 @@
             <div class="title login">
                Login Form
             </div>
-            <div class="title signup">
-               SignUp Form
-            </div>
          </div>
          <div class="form-container">
-            <div class="slide-controls">
-               <input type="radio" name="slide" id="login" checked>
-               <input type="radio" name="slide" id="signup">
-               <label for="login" class="slide login">Login</label>
-               <label for="signup" class="slide signup">Signup</label>
-               <div class="slider-tab"></div>
-            </div>
             <div class="form-inner">
 
                <!-- Login Section -->
@@ -43,28 +33,7 @@
                      <input type="submit" value="Login">
                   </div>
                   <div class="signup-link">
-                     Not a member? <a href="">SignUp Now</a>
-                  </div>
-               </form>
-
-               <!-- SignUp Section -->
-
-               <form action="signup.php" method="POST" class="signup">
-               <div class="field">
-                     <input type="name" placeholder="User Name" name="name" required>
-                  </div>
-                  <div class="field">
-                     <input type="email" placeholder="Email Address" name="email" required>
-                  </div>
-                  <div class="field">
-                     <input type="password" placeholder="Password" name="password" required>
-                  </div>
-                  <div class="field btn">
-                     <div class="btn-layer"></div>
-                     <input type="submit" value="Signup">
-                  </div>
-                  <div class="signup-link">
-                     Have an account? <a href="">Login Now</a>
+                     Not a member? <a href="user_signup.php">SignUp Now</a>
                   </div>
                </form>
             </div>
@@ -74,19 +43,10 @@
          const loginText = document.querySelector(".title-text .login");
          const loginForm = document.querySelector("form.login");
          const loginBtn = document.querySelector("label.login");
-         const signupBtn = document.querySelector("label.signup");
-         const signupLink = document.querySelector("form .signup-link a");
-         signupBtn.onclick = (()=>{
-           loginForm.style.marginLeft = "-50%";
-           loginText.style.marginLeft = "-50%";
-         });
+
          loginBtn.onclick = (()=>{
            loginForm.style.marginLeft = "0%";
            loginText.style.marginLeft = "0%";
-         });
-         signupLink.onclick = (()=>{
-           signupBtn.click();
-           return false;
          });
       </script>
    </body>
